32de526d733ee173e8ecee4244c96165c531afe2
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2011-09-02 21:57:48 +0530

    Implement a CGI based application on top of the API from apropos-utils for searching from the browser and using the Mongoose web server for making the CGI run. Mongoose is an embedded web server which does not require a separate server process.

--- Makefile
+++ Makefile
@@ -3,9 +3,10 @@
 MDIST=	${NETBSDSRCDIR}/external/bsd/mdocml/dist
 MDOCDIR=${NETBSDSRCDIR}/external/bsd/mdocml
 
-PROGS=	makemandb apropos
+PROGS=	makemandb apropos apropos_cgi
 SRCS.makemandb=		makemandb.c sqlite3.c apropos-utils.c apropos-utils.h stopword_tokenizer.c stopword_tokenizer.h
 SRCS.apropos=	apropos.c sqlite3.c apropos-utils.c apropos-utils.h stopword_tokenizer.c stopword_tokenizer.h
+SRCS.apropos_cgi=	apropos_cgi.c mongoose.h mongoose.c apropos-utils.c apropos-utils.h sqlite3.c sqlite3.h stopword_tokenizer.h stopword_tokenizer.c
 MAN=	makemandb.1 apropos.1 apropos-utils.3 init_db.3 close_db.3 run_query.3 run_query_html.3 run_query_pager.3
 
 .PATH:	${MDIST}
@@ -18,6 +19,7 @@ MDOCMLLIB=	${MDOCMLOBJDIR}/libmandoc.a
 
 DPADD.makemandb+= 	${MDOCMLLIB}
 LDADD.makemandb+= 	-L${MDOCMLOBJDIR} -lmandoc
+LDADD.apropos_cgi+=	-lpthread
 LDADD+=	-lm
 LDADD+=	-lz
 LDADD+=	-lutil
---
+++ apropos_cgi.c
@@ -0,0 +1,80 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "apropos-utils.h"
+#include "mongoose.h"
+
+#define BUFLEN 512
+
+typedef struct apropos_data {
+	struct mg_connection *conn;
+	int count;
+} apropos_data;
+
+static const char *standard_reply = "HTTP/1.1 200 OK\r\n"
+  "Content-Type: text/html\r\n"
+  "Connection: close\r\n\r\n";
+
+static const char *html_template = "<html>\n<head>\n<title>\napropos\n</title>"
+	"</head>\n<body>\n";
+
+static int
+apropos_callback(void *data, int ncol, char **col_values, char **col_names)
+{
+	apropos_data *ap_data = (apropos_data *) data;
+	ap_data->count++;
+	mg_printf(ap_data->conn, "%s\n", col_values[0]);
+	return 0;
+}
+
+static void *
+callback(enum mg_event event, struct mg_connection *conn,
+		const struct mg_request_info *request_info)
+{
+	char query[BUFLEN];
+	char *errmsg;
+	query_args args;
+	const char *sec_nums[] = {0,0,0,0,0,0,0,0,0};
+	apropos_data ap_data;
+	ap_data.conn = conn;
+	ap_data.count = 0;
+	sqlite3 *db;
+	assert(request_info);
+
+	if (event == MG_NEW_REQUEST ) {
+		fprintf(stderr, "%s\n", request_info->uri);
+		if (strcmp(request_info->uri, "/apropos") ==0 ) {
+			mg_printf(conn, "%s", standard_reply);
+			if (mg_get_var(request_info->query_string, strlen(request_info->query_string), 
+				"q", query, BUFLEN) == -1)
+				return NULL;
+			
+			db = init_db(DB_READONLY);
+			args.search_str = query;
+			args.sec_nums = sec_nums;
+			args.nrec = 10;
+			args.callback = &apropos_callback;
+			args.callback_data = &ap_data;
+			args.errmsg = &errmsg;
+			run_query_html(db, &args);
+			close_db(db);
+			mg_printf(conn, "</body></html>");
+			return (void *)"";	// Mark as processed*/
+		}
+		else
+			return (void *)NULL;
+	}
+	return NULL;
+}
+
+int
+main(void)
+{
+	struct mg_context *ctx;
+	const char *options[] = {"document_root", "./www", "listening_ports", "8080", NULL};
+	ctx = mg_start(&callback, NULL, options);
+	getchar();	// Wait until user hits "enter"
+	mg_stop(ctx);
+	return 0;
+}
